8a30154d012a440607f85ccfeca54c3d96317467
elad c1 <eladc17@gmail.com>
2019-05-25 23:32:16 +0300

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GroupComponent } from './group/group.component';
-import { ListComponent } from './list/list.component';
-import { ItemComponent } from './item/item.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule, MatIconModule, MatListModule} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GroupComponent,
-    ListComponent,
-    ItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
